1f2f06f935af9b9b108098e701f1ff803f1f35ab
kajtargo <kajetan.targonski@gmail.com>
2017-12-06 00:57:59 +0100

    Day 5 - part 2

--- AOC05/app.py
+++ AOC05/app.py
@@ -1,5 +1,7 @@
 import copy
 
+EXAMPLE = '0 3 0 1 -3'
+
 def read_instructions(filepath):
     with open(filepath) as f:
         content = f.read()
@@ -18,10 +20,33 @@ def run_instructions(instructions):
         if position >= length:
             return steps_count
 
+def run_weird_instructions(instructions):
+    length = len(instructions)
+    position = 0
+    steps_count = 0
+
+    while True:
+        jump = int(instructions[position])
+
+        if jump >= 3:
+            instructions[position] -= 1
+        else:
+            instructions[position] += 1
+
+        steps_count += 1
+        position += jump
+
+        if position >= length:
+            return steps_count
 
 
 if __name__ == '__main__':
     instructions_list = read_instructions('./AOC05/input.io')
 
+    # movements_count = run_instructions(map(lambda x: int(x), EXAMPLE.split()))
     movements_count = run_instructions(copy.copy(instructions_list))
     print 'Movements count: {0}'.format(movements_count)
+
+    # movements_count = run_weird_instructions(map(lambda x: int(x), EXAMPLE.split()))
+    movements_count = run_weird_instructions(copy.copy(instructions_list))
+    print 'Weird movements count: {0}'.format(movements_count)
