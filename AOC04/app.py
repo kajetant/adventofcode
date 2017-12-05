d16390efe3720b9d6648b76aec614eb594d4cd97
kajtargo <kajetan.targonski@gmail.com>
2017-12-06 00:20:27 +0100

    Day 4 - part 2

--- AOC04/app.py
+++ AOC04/app.py
@@ -6,7 +6,23 @@ def read_passphrases(filepath):
 def passphrase_has_no_duplicates(passphrase):
         return len(passphrase) == len(set(passphrase))
 
+def passphrase_has_no_anagrams(passphrase):
+    lenght = len(passphrase)
+    for i in range(0, lenght - 1):
+        sorted_a = sorted(passphrase[i])
+
+        for j in range(i + 1, lenght):
+            sorted_b = sorted(passphrase[j])
+            if sorted_a == sorted_b:
+                return False
+
+    return True
+
+
 if __name__ == "__main__":
     input = read_passphrases('./AOC04/input.txt')
-    result = reduce(lambda x,y: x+y, map(lambda p: 1 if passphrase_has_no_duplicates(p) == True else 0, input))
-    print 'Valid passphrases without duplicates: {0}'.format(result)
+    no_duplicates_count = reduce(lambda x,y: x+y, map(lambda p: 1 if passphrase_has_no_duplicates(p) == True else 0, input))
+    print 'Valid passphrases without duplicates: {0}'.format(no_duplicates_count)
+
+    no_anagrams_count = reduce(lambda x,y: x+y, map(lambda p: 1 if passphrase_has_no_anagrams(p) == True else 0, input))
+    print 'Valid passphrases without anagrams: {0}'.format(no_anagrams_count)
